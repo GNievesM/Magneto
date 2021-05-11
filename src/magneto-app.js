862172d4d6361e6388e99e4e5281abc4d1a9627a
Gaston <gaston.nieves@hotmail.com>
2021-05-11 19:35:16 -0300

    Clean up magneto-app js
    Deleted unnecesary commentaries

--- src/magneto-app.js
+++ src/magneto-app.js
@@ -4,23 +4,7 @@ import routes from "./routes/DnaRoutes.js"
 const app = express();
 const port = 3000;
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
-
-//app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
-//app.use(express.json({type:'application/x-www-form-urlencoded'}));
-//app.use(express.raw());
 app.use('/', routes);
 
 app.listen(port,()=> console.log(`Listening on port ${port}!`));
